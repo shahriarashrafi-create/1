f4e979651f4f6bfa8ac56b23b045565d509c9aa4
shahriarashrafi-create <shahriar.ashrafi@gmail.com>
2026-09-01 21:06:17 +0330

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,5 @@
+@rem Gradle startup script for Windows
+@echo off
+set DIRNAME=%~dp0
+set CLASSPATH=%DIRNAME%\gradle\wrapper\gradle-wrapper.jar
+java -Xmx64m -Xms64m -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
